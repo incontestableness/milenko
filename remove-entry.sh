bfa2570de6eae10889b42b26992897e077b35c15
incontestableness <66852150+incontestableness@users.noreply.github.com>
2020-09-29 20:42:58 -0500

    Update remove-entry.sh

--- remove-entry.sh
+++ remove-entry.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This is very inconvenient but it works for now and only takes a few minutes so, eh
+# This is a bit inconvenient but it works for now and only takes a few minutes so, eh
 
 
 read -p "Be sure you've stopped the bots with ./stop first. Press Enter to continue."
@@ -8,17 +8,29 @@ read -p "Be sure you've stopped the bots with ./stop first. Press Enter to conti
 read -p "Please stop the running live-scrape instance, if any. Press Enter to continue."
 tmux a -t live-scrape
 
+# Delete all cathook logs so the entry doesn't get picked back up
+pushd /tmp
+rm cathook-*.log
+popd
+
 # Delete the cathook plist file since I am NOT editing that binary crap from here
+# and we don't want bots to just load that entry back in, log it, and have it added to the list again
 rm /opt/cathook/data/plist
 
-# Remove the entry
+# Remove the entry from the nsv file so bots don't load it from there, and so the playerlist doesn't include it
 cat catlist.nsv | grep -v $1 >catlist_new.nsv
 
-# Update
+# Update database
 mv catlist_new.nsv catlist.nsv
 git add catlist.nsv
 git commit -m "Remove an entry"
 
 # Verify
 git show
-echo "Ready to push commit..."
+
+# Update playerlist
+./update.py
+
+# Verify
+git show
+echo "Ready to push commits..."
