d9b3f0deefc2c8dbd81c522e103721781b504377
incontestableness <66852150+incontestableness@users.noreply.github.com>
2020-10-24 00:41:17 -0500

    Add a script to convert catlist.nsv to Steam64 IDs

---
+++ 3-to-64.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3.8
+
+# Lazy script to convert Steam ID3 to Steam64 ID
+
+import os
+import subprocess
+
+
+print("This will take a long time, be patient.")
+
+file = open("catlist.nsv", "r")
+data = file.read()
+file.close()
+
+to_convert = data.split("\n")
+to_convert.remove("")
+converted = []
+for index, id3 in enumerate(to_convert):
+	output = subprocess.check_output(f"curl https://steamid.xyz/{id3} 2>&1 | grep Steam64 -A1", shell=True)
+	output = output.decode("UTF-8")
+	temp = output[output.find("value=\"") + 7:]
+	id64 = temp[:temp.find("\">")]
+	converted.append(id64)
+	print(f"Converted {index + 1}/{len(to_convert)}...")
+
+try: os.remove("catlist.nsv.64")
+except: pass
+file = open("catlist.nsv.64", "a")
+for id64 in converted:
+	print(id64)
+	file.write(f"{id64}\n")
+file.close()
+
+print("Wrote converted list to catlist.nsv.64")
